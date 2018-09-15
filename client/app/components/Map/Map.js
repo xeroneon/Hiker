99202c2643b741762566e175f256a869244f571b
Jacob <jrnels10@gmail.com>
2018-09-14 20:29:21 -0700

    need to revert back

--- client/app/components/Map/Map.js
+++ client/app/components/Map/Map.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
 
-const divStyle = {
-    height: "50vh",
-    width: "100vw",
-    zIndex: 2
-};
+const React = window.React
+const { Map: LeafletMap, TileLayer, Marker, Popup } = window.ReactLeaflet
+const position = [51.505, -0.09]
+const Example = () => (
+  <LeafletMap center={position} zoom={13}>
+    <TileLayer
+      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        <span>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </span>
+      </Popup>
+    </Marker>
+  </LeafletMap>
+)
+// window.ReactDOM.render(<Example />, document.getElementById('container'))
 
-class Map extends Component {
+// import React, { Component } from 'react';
 
-    render() {
-        return (
-            <>
-            <div className="azMap h-100 w-100">
-                <div id="map" style={divStyle}>
-                </div>
-                <div className="newTrail">
-                </div>
-            </div>
-            </>
-        );
-    }
+// const divStyle = {
+//     height: "50vh",
+//     width: "100vw",
+//     zIndex: 2
+// };
 
-}
+// class Map extends Component {
 
-export default Map;
+//     render() {
+//         return (
+//             <>
+//             <div className="azMap h-100 w-100">
+//                 <div id="map" style={divStyle}>
+//                 </div>
+//                 <div className="newTrail">
+//                 </div>
+//             </div>
+//             </>
+//         );
+//     }
+
+// }
+
+export default Example;
