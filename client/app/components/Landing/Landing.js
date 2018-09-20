1321b869e4ac6ae3d55d3d3dd58e5d7ddd25b863
Andrew Vasquez <andrewvasquez0113@gmail.com>
2018-09-19 18:59:07 -0700

    now if you are signed in you will be redirected to home page

--- client/app/components/Landing/Landing.js
+++ client/app/components/Landing/Landing.js
@@ -4,16 +4,38 @@ import 'whatwg-fetch';
 // import { url } from 'inspector';
 // import '../../styles/landing.scss';
 import Button from '../Button/Button'
+import {
+    getFromStorage,
+    setInStorage
+} from "../../utils/storage";
+import { Redirect } from 'react-router'
+
 
 
 class Landing extends Component {
     
     constructor(props) {
         super(props);
+
+        this.state = {
+            redirect: false
+        }
     }
     
     render() {
         document.body.style = "height: 100vh; width: 100vw; background: url('/assets/img/hiking.jpg') center center no-repeat !important; background-size: cover !important;";
+
+        const token = getFromStorage("Hiker");
+
+        if (token) {
+            this.setState({
+                redirect: true
+            })
+        }
+
+        if (this.state.redirect) {
+            return <Redirect to='/Home' />;
+        }
     return(
       <div className="landingDiv">
           <h2><strong>Helping hikers stay safe</strong></h2>
