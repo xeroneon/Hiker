10c4aa7a46236a17a050e0bfb6201a8e2212f2bc
Andrew Vasquez <andrewvasquez0113@gmail.com>
2018-09-19 18:46:36 -0700

    sign in and sign up now work

--- client/app/index.js
+++ client/app/index.js
@@ -14,6 +14,7 @@ import NotFound from './components/App/NotFound';
 import Home from './components/Home/Home';
 import Landing from './components/Landing/Landing';
 import SignUp from './components/SignUp/SignUp';
+import SignIn from './components/SignIn/SignIn';
 
 import './styles/styles.scss';
 
@@ -23,6 +24,7 @@ render((
       <Switch>
         <Route exact path="/" component={Landing}/>
         <Route exact path="/signup" component={SignUp}/>
+        <Route exact path="/signin" component={SignIn}/>
         <Route exact path="/Home" component={Home}/>
         <Route component={NotFound}/>
       </Switch>
