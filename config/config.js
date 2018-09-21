584ab53481fc669f1b3ef937af8d1313b6c15a08
Xeroneon <andrewvasquez0113@gmail.com>
2018-09-21 09:05:40 -0700

    changed config for gitlab

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'ds211143.mlab.com:11143/hiker -u Xeroneon -p Llawliet1!',
+  db: 'mongodb://Xeroneon:Llawliet1!@ds211143.mlab.com:11143/hiker',
   db_dev: 'mongodb://localhost:27017/reacttest',
 };
