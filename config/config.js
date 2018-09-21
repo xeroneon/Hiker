a31a8273d2b80a92b64f8628bd43f8c8e26c2031
Xeroneon <andrewvasquez0113@gmail.com>
2018-09-21 09:19:23 -0700

    changed config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://Xeroneon:Llawliet1!@ds211143.mlab.com:11143/hiker',
+  db: process.env.MONGODB_URI,
   db_dev: 'mongodb://localhost:27017/reacttest',
 };
