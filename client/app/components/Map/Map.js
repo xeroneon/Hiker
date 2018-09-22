87eee5893836a88fc76a2e3db03535d9b1a4c38c
Jacob <jrnels10@gmail.com>
2018-09-21 18:43:23 -0700

    comments on map.js

--- client/app/components/Map/Map.js
+++ client/app/components/Map/Map.js
@@ -15,33 +15,43 @@ import Locate from './Locate';
 
 class Map extends Component {
 
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      something: ''
-    }
-  }
+  //   this.state = {
+  //     something: ''
+  //   }
+  // }
 
   componentDidMount() {
     let map = L.map("map").fitWorld();
-    map.locate({ setView: true, maxZoom: 10 });
-  
-    Locate(map);
-
     let state = {
       lat: 33.5,
       lon: -112
     };
-    TrailsAPI.getTrailsInArea(state).then(function (data) {
-      // trailData = data;
-      trailsInArea(data)
-    })
     let trailInfo = {
       map: map,
       data: []
     }
 
+
+    map.locate({ setView: true, maxZoom: 10 });
+
+    // Creates button and when clicked, locates the user and zooms in to their location
+    Locate(map);
+
+    // add Basemap component to map.
+    Basemap.addTo(map)
+
+    // on initial page load,
+    // TrailsAPI.getTrailsInArea pulls 100 trails around the user. Then runs the function
+    // trailsInArea with the data.
+    TrailsAPI.getTrailsInArea(state).then(function (data) {
+      // trailData = data;
+      trailsInArea(data)
+    })
+
+    // finds the latlon of the center of map and runs TrailsAPI.
     function onLocation() {
       state.lat = map.getCenter().lat;
       state.lon = map.getCenter().lng
@@ -51,12 +61,14 @@ class Map extends Component {
         trailsInArea(data)
       })
     }
+
+    // when user drags the map, it runs the function onLocation.  
     map.on('dragend', function (e) {
       onLocation(e)
     })
 
-    Basemap.addTo(map)
-
+    // creates markers and popups for all the points. 
+    // and adds a clickable function for it. 
     function trailsInArea(data) {
       // console.log(data)
       trailInfo.data = data.data;
@@ -76,21 +88,21 @@ class Map extends Component {
 
   render() {
     return (
-        <div className="azMap">
-          <div id="map-wrapper">
-            <div id="map">
+      <div className="azMap">
+        <div id="map-wrapper">
+          <div id="map">
 
-              <div id="button-wrapper">  legend
+            <div id="button-wrapper">  legend
                 </div>
-            </div>
-
-
-
-
           </div>
-          <div className="newTrail">
-          </div>
+
+
+
+
         </div>
+        <div className="newTrail">
+        </div>
+      </div>
 
     );
   }
