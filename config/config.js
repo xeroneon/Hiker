7eefd57334bea8ff15b04b59babe7c7cafc50223
coxipozina <coxipozina@gmail.com>
2018-09-21 18:19:08 -0700

    merge conflicts

--- config/config.js
+++ config/config.js
@@ -1,12 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-<<<<<<< HEAD
-    db: 'mongodb://username:password@url:port/db',
-    db_dev: 'mongodb://localhost:27017/reacttest',
-  };
-=======
   db: process.env.MONGODB_URI,
   db_dev: 'mongodb://localhost:27017/reacttest',
 };
->>>>>>> a1122fca59b40c4d676af4f22ba4b7b7b83ff020
