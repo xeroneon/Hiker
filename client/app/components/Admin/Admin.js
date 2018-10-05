3d3f1fad67b0211634478e23fd1bd830e99c6d7a
Xeroneon <andrewvasquez0113@gmail.com>
2018-10-05 14:49:26 -0700

    added admin stuff and fixed map

--- client/app/components/Admin/Admin.js
+++ client/app/components/Admin/Admin.js
@@ -7,6 +7,7 @@ import {
 import { Redirect } from 'react-router';
 import Nav from "../Nav/Nav";
 import axios from "axios";
+import moment from "moment";
 
 class Admin extends Component {
 
@@ -17,33 +18,63 @@ class Admin extends Component {
   // const userAdmin = getFromStorage("userAdmin")
   // console.log(userAdmin)
 
-  componentWillMount = () => {
+  componentDidMount = () => {
     axios.get(`/api/get-user?token=${this.state.token}`)
       .then(res => {
-        if(res.data.success && res.data.user.role === 99) {
+        if (res.data.success && res.data.user.role === 99) {
           this.setState({
             auth: true
           })
         }
+      })
+
+    axios.get(`/api/get-all-users`)
+      .then(res => {
+        this.setState({
+          data: res.data.data
+        })
+
+        console.log(res.data.data);
       })
   }
 
   render() {
 
     if (this.state.auth) {
-  
-  
+
+
       return (
 
         <div className="admin">
-          <Nav token={this.state.token} admin={this.state.auth}/>
+          <Nav token={this.state.token} admin={this.state.auth} />
           <Map />
-        
+          <div className="container users-container">
+            <h1 className="text-white">Users Checked In</h1>
+            <div className="row">
+
+              {this.state.data && this.state.data.map(data => {
+                return (
+                  <div className="col-sm-3" key={data._id}>
+                    <div className="users card">
+                      <div className="card-body">
+                        <h3 className="card-title">Name: <br/>{data.firstName} {data.lastName}</h3>
+                        <br/>
+                        <h3 className="card-title">Trail: <br/>{data.trails[data.trails.length - 1].name}</h3>
+                        <br/>
+                        <h3 className="card-title">Complete Time:<br/>{moment(data.trails[data.trails.length - 1].completetime).format("MMMM Do, h:mm a")}</h3>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+
         </div>
-      
+
       )
     }
-  
+
     else {
       return ("You do not have permission to access this page")
     }
